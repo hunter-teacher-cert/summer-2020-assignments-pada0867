a6ff2b27de8c4f6990e200428aea5890532dfcdf
Jack Padalino <pada0867@gmail.com>
2020-07-01 21:23:48 -0400

    Chapter 2 Exercise 2 redo

---
+++ pre02/Date_redo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+public class Date_redo {
+  public static void main(String[] args){
+    String day = "Wednesday";
+    int date = 01;
+    String month = "July";
+    int year = 2015;
+    System.out.println("American format: ");
+    System.out.println(day + ", " + month + " " + date + ", " + year);
+    System.out.println("European format: ");
+    System.out.println(day + ", " + date + " " + month + ", " + year);
+
+  }
+}
