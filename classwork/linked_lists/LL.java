dab26344cac3c2ddadc2c711849631f799fd4408
Jack Padalino <pada0867@gmail.com>
2020-07-08 15:45:52 -0400

    Linked list class day2day 07-07

--- classwork/linked_lists/LL.java
+++ classwork/linked_lists/LL.java
@@ -4,32 +4,78 @@ import java.util.*;
 public class LL{ //when you instantiate this class in the driver, you create a new linked list object that has all the following attributes and can run the following methods
 
   private Node head; //when you make a new linked list, the list will automatically come with a head Node
+  private int length;
 
   public LL(){ //linked list constructor - creates a head node that is null (has no value). The head is the very end of the list.
     head = null;
+    length = 0;
   }
 
   // tests to see if a list is empty. If empty returns true, if filled returns false.
   public boolean isEmpty(){
-    if(head==null){ //checks to see if the first
-      return true;
+    return head==null;
+  }
+
+  public int length(){
+    return length;
+  }
+
+  //inserts data at specified index
+  public void insert(String data, int index){
+    Node new_node = new Node(data);
+    Node temp = head;
+    int count = 0;
+    if (index==0){
+      add(data);
     }
     else{
-      return false;
+      index-=1;
+      while(temp!=null){
+        if(count == index){
+          new_node.setNext(temp.getNext());
+          temp.setNext(new_node);
+          length+=1;
+        }
+        count++;
+        temp = temp.getNext();
+      }
     }
   }
 
-  //returns the data at a specified index
-
-  public String getData(int index){
-    Node current = head;
+// this remove method traverses the linked list until it arrives at the node precedining
+// the node I want to remove. It then sets the preceding node's 'next' to the
+// node that is two away in the list. It forms a link that goes around the
+// node we want to remove. Really the node we want to remove isn't REALLY
+// removed, the preceding node steps around it.
+  public void remove(int index){
+    Node temp = head;
     int count = 0;
-
-    while(current!=null){
-      if (count == index){
-        return current.data;
+    if (index==0){
+      head = head.getNext();
+      length-=1;
+    }
+    else{
+      index-=1;
+      while(temp!=null){
+        if(count == index){
+          temp.setNext(temp.getNext().getNext());
+          length-=1;
+        }
+        count++;
+        temp = temp.getNext();
+        }
       }
-      current = current.getNext();
+    }
+
+  //returns the data at a specified index
+  public String getData(int index){
+    Node temp = head;
+    int count = 0;
+    while(temp!=null){
+      if (count == index){
+        return temp.data;
+      }
+      temp = temp.getNext();
       count++;
     }
     return "N/A";
@@ -38,19 +84,17 @@ public class LL{ //when you instantiate this class in the driver, you create a n
   // adds a new node to the beginning of a linked list
   public void add(String data){
     Node new_node = new Node(data);
-
     //point the new node towards the head of the list
     new_node.setNext(head);
-
     //make the new node the "head" of the list, the next new_node will point towards this new head
     head = new_node;
+    length+=1;
   }
 
   //prints out all the nodes of a linked list
   public String toString(){
     String s = "";
     Node temp = head;
-    temp = head;
 
     //this loop will iterate through the linked list and print out the data that is stored in all the nodes
     while(temp != null){
