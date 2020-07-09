464756dcda794f97935805dc7e3e2a7c9c5fbade
Jack Padalino <pada0867@gmail.com>
2020-07-09 08:13:06 -0400

    Linked list Driver class 07-08

--- classwork/linked_lists/LLD.java
+++ classwork/linked_lists/LLD.java
@@ -19,6 +19,8 @@ public class LLD{
     System.out.println(ll_one);
     ll_one.remove(3);
     System.out.println(ll_one);
+    ll_one.set("Versa",1);
+    System.out.println(ll_one);
     System.out.println(ll_one.length());
     System.out.println(ll_one.isEmpty());
 
