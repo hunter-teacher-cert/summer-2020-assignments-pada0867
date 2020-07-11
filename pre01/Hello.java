f00795524213a12b12e39c92b59bb756fc09fe5d
Jack Padalino <pada0867@gmail.com>
2020-07-11 17:06:01 -0400

    First Java program

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -2,6 +2,6 @@ import java.io.*;
 import java.util.*;
 public class Hello {
   public static void main(String[] args){
-    System.out.println("Whats up my dude?!");
+    System.out.println("Hello world!");
   }
 }
