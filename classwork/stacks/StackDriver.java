241ef919576757c6cc4fed7993f5b97d204727fa
Jack Padalino <pada0867@gmail.com>
2020-07-12 10:32:21 -0400

    Stack Driver

---
+++ classwork/stacks/StackDriver.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class StackDriver{
+
+  public static void main(String[] args){
+
+    Stack stack = new Stack();
+    System.out.println(stack.isEmpty());
+    stack.push("A");
+    stack.push("B");
+    stack.push("C");
+    System.out.println(stack);
+    System.out.println(stack.isEmpty());
+    System.out.println(stack.size());
+    stack.pop();
+    System.out.println(stack);
+    System.out.println(stack.size());
+
+  }
+
+}
